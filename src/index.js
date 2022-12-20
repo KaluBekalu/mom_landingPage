aeca7aecab50949ca728d030bdc6facf6ffa581c
kalubekalu <kalubekalu1@gmail.com>
2022-12-20 12:55:59 +0300

    added slides and animation

--- src/index.js
+++ src/index.js
@@ -126,3 +126,38 @@ const startCountDown = () => {
 };
 
 startCountDown();
+
+// slider
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function currentDiv(n) {
+  showDivs((slideIndex = n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("slider-controller");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(
+      " slider-controller-active",
+      ""
+    );
+  }
+  x[slideIndex - 1].style.display = "grid";
+  dots[slideIndex - 1].className += " slider-controller-active";
+}
